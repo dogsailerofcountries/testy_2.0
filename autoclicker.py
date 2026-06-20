867aa210d21a0035cc66cfded4333e7dddfdfd3c
sammy <sammyabril2@gmail.com>
2026-06-19 21:12:27 -0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,11 @@
-import win32api
+import time
 
-print("helloworld")
+import win32api
+import win32con
+
+x, y = win32api.GetCursorPos()
+
+
+def click():
+    win32api.mouse_event(win32con.MOUSEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEVENTF_LEFTUP, x, y, 0, 0)
