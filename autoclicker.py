1d1eaa8803e5772f1ef539155bea627976302b95
sammy <sammyabril2@gmail.com>
2026-06-19 22:22:37 -0500

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -5,16 +5,21 @@ import win32api
 import win32con
 
 running = True
-
 x, y = win32api.GetCursorPos()
 
 
 def click():
     while running:
-        win32api.mouse_event(win32con.MOUSEVENTF_LEFTDOWN, x, y, 0, 0)
-        time.wait(0.02)
-        win32api.mouse_event(win32con.MOUSEVENTF_LEFTUP, x, y, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+        time.sleep(0.02)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
 
-with pynput.keyboard.GlobalHotKeys({"<ctrl>+<alt>+f": click}) as h:
+def stop():
+    running = False
+
+
+with pynput.keyboard.GlobalHotKeys(
+    {"<ctrl>+<alt>+f": click, "<ctrl>+<alt>+g": stop}
+) as h:
     h.join()
